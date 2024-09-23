95cb056528694b88c708e5c3f9899e093c4fa801
Stefan Uebe <stefan@vaadin.com>
2024-09-23 08:54:46 +0200

    Added test

--- src/test/java/org/vaadin/addons/componentfactory/ComboBoxLightTest.java
+++ src/test/java/org/vaadin/addons/componentfactory/ComboBoxLightTest.java
@@ -1,5 +1,8 @@
 package org.vaadin.addons.componentfactory;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -16,5 +19,15 @@ public class ComboBoxLightTest {
     public static final Data TEST_DATA = new Data(1);
     public static final Entity TEST_ENTITY = new Entity(1);
 
+    @Test
+    public void test_preventDataProviderNPE() {
+        ComboBoxLight<Entity> light = new ComboBoxLight<>();
+
+        try {
+            light.setItemLabelGenerator(item -> "unused");
+        } catch (NullPointerException e) {
+            Assert.fail("NullPointerException was thrown using setItemLabelGenerator");
+        }
+    }
 
 }

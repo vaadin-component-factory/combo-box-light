cf0c21342ef25a4e87bcfde84cafe8ec3e95e885
Stefan Uebe <stefan@vaadin.com>
2024-09-23 07:36:37 +0200

    Preparations for JUnit tests

--- src/test/java/org/vaadin/addons/componentfactory/CompView.java
+++ src/test/java/org/vaadin/addons/componentfactory/CompView.java
@@ -1,7 +1,6 @@
 package org.vaadin.addons.componentfactory;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,62 +17,38 @@ public class CompView extends VerticalLayout {
     }
 
     private void inMemory() {
-        ComboBoxLight<Data> comboBox = new ComboBoxLight<>();
+        ComboBoxLight<Entity> comboBox = new ComboBoxLight<>();
         comboBox.setLabel("Values");
 
-        List<Data> values = IntStream.range(0, 250)
-                .mapToObj(Data::new)
+        List<Entity> values = IntStream.range(0, 250)
+                .mapToObj(Entity::new)
                 .collect(Collectors.toList());
 
         comboBox.setItems(values);
 
         initItemLabelGenerator(comboBox);
 
-        comboBox.setValue(new Data(123));
+        comboBox.setValue(new Entity(123));
         comboBox.addValueChangeListener(e -> Notification.show(asUserReadable(e.getValue())));
 
 
         add(comboBox);
     }
 
-    private static void initItemLabelGenerator(ComboBoxLight<Data> comboBox) {
+    private static void initItemLabelGenerator(ComboBoxLight<Entity> comboBox) {
         comboBox.setItemLabelGenerator(item -> asUserReadable(item));
     }
 
-    private static void initLitRenderer(ComboBoxLight<Data> comboBox) {
+    private static void initLitRenderer(ComboBoxLight<Entity> comboBox) {
         comboBox.setRenderer(
-                LitRenderer.<Data> of("<span style='color: var(--lumo-secondary-text-color);'>" +
+                LitRenderer.<Entity> of("<span style='color: var(--lumo-secondary-text-color);'>" +
                                       "<b>${item.value}</b></span>")
                         .withProperty("value", item -> asUserReadable(item)));
     }
 
-    private static String asUserReadable(Data item) {
+    private static String asUserReadable(Entity item) {
         return "Item " + item.getId();
     }
 
 
-    public static class Data {
-        private final int id;
-
-        public Data(int id) {
-            this.id = id;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Data data = (Data) o;
-            return id == data.id;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(id);
-        }
-    }
 }

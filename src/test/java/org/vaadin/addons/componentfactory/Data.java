8e956f16f1c42932037cad39f64b8373fe46d9aa
Stefan Uebe <stefan@vaadin.com>
2024-09-23 07:35:22 +0200

    Preparations for JUnit tests

---
+++ src/test/java/org/vaadin/addons/componentfactory/Data.java
@@ -0,0 +1,15 @@
+package org.vaadin.addons.componentfactory;
+
+public class Data {
+    private final int id;
+
+    public Data(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+}

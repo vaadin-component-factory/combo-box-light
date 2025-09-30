6da854b3aeda98a111fc306c2ad9668bfa03ec6b
Stefan Uebe <stefan@vaadin.com>
2025-09-30 13:36:20 +0200

    Minor changes

---
+++ src/test/java/org/vaadin/addons/componentfactory/AppShellConfig.java
@@ -0,0 +1,9 @@
+package org.vaadin.addons.componentfactory;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@Theme(themeClass = Lumo.class)
+public class AppShellConfig implements AppShellConfigurator {
+}
